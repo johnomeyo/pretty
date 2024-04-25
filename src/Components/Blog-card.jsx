532cf95d4eb9cc38f822797ce941c80bac5c7afc
John Omeyo <johnomeyo@gmail.com>
2024-04-25 15:56:41 +0300

    Blogs page

---
+++ src/Components/Blog-card.jsx
@@ -0,0 +1,14 @@
+import "../Components/Blog.css";
+
+export const BlogCard = () => {
+  return (
+    <div className="blog-card">
+      <h4>Photography and videography</h4>
+      <p>
+        Creating a lasting visual impression on a potential customer usually
+        happens with an image first. Be it still photography or videography,
+        motion graphics or compeling combination of all the above
+      </p>
+    </div>
+  );
+};
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,183 +1,19 @@
+import "../Components/Blog.css";
+import { BlogCard } from "../Components/Blog-card";
 export const Blog = () => {
   return (
     <>
-          <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p></p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vitae
-        illo libero, eveniet modi sed placeat nobis quae accusamus molestiae
-        odio, distinctio molestias vel commodi deserunt amet porro incidunt
-        fugit.
-      </p>
-      
+      <h2>Our Blogs</h2>
+      <div className="blog-container">
+        <div className="blog">
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+        </div>
+      </div>
     </>
   );
 };
