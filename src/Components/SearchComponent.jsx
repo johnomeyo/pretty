947be2ae077a7e017d89a225b6d0fc14812239c1
John Omeyo <johnomeyo@gmail.com>
2024-05-10 23:51:37 +0300

    search page fetch

--- src/Components/SearchComponent.jsx
+++ src/Components/SearchComponent.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { SearchSuggestion } from "./Search-Suggestions";
 import SearchImage from "../Assets/search.png";
+import axios from "axios";
 export const SearchBar = () => {
   const suggestions = [
     "Street photography",
@@ -10,6 +11,18 @@ export const SearchBar = () => {
     "Relaxing images",
   ];
 
+  const fetchImages = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.unsplash.com/photos/?client_id=WFZszaTgc3XOPK56UwAq_Bizq-U3CNY9n604h3bbdn4"
+      );
+      setImages(response.data[0].urls);
+      console.log(images);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const [images, setImages] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [filteredSuggestions, setFilteredSuggestions] = useState([]);
 
@@ -19,24 +32,41 @@ export const SearchBar = () => {
     );
     setFilteredSuggestions(filtered);
   }, [inputValue]);
+  let gridItems = [];
+  if (images.length > 0) {
+    let gridItems = images.map((gridItem, index) => {
+      gridItems = gridItems;
+      return (
+        <div className="grid-item" key={index}>
+          <img src={gridItem.small} alt="" />
+        </div>
+      );
+    });
+  }
+
   return (
     <div className="search-container">
       <div className="search-bar">
-        <form action="">
-          <input
-            type="text"
-            name="search"
-            placeholder="Nature"
-            autoComplete="on"
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button type="submit">{`Search`}</button>
-        </form>
+        <input
+          type="text"
+          name="search"
+          placeholder="Nature"
+          autoComplete="on"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={fetchImages}>{"Search"}</button>
       </div>
-      {inputValue === "" ? (
-        <img src={SearchImage} alt="" />
+
+      {images.length === 0 ? (
+        <div>
+          {inputValue === "" ? (
+            <img src={SearchImage} alt="" />
+          ) : (
+            <SearchSuggestion suggestions={filteredSuggestions} />
+          )}
+        </div>
       ) : (
-        <SearchSuggestion suggestions={filteredSuggestions} />
+        <div className="grid-container">{gridItems}</div>
       )}
     </div>
   );
